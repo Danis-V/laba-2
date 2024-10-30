34e58ac931b86aba166e733b6a708dc102dfb4ab
Danis-V <danis.vakhitov.06@gmail.com>
2024-10-30 16:42:23 +0500

    Create examples_11.py

---
+++ examples/examples_11.py
@@ -0,0 +1,6 @@
+print("Функция выводящая список книг:")
+def book_list(books, func):
+    for book in books:
+        print(func(book))
+books = ['System Design','Python и DevOps','Git. Практическое руководство','book of bash']
+book_list(books, lambda book: book.upper() + ' - прочитано')

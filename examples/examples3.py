36fe719fa224920291d8cf494cf4d46655381331
Danis-V <danis.vakhitov.06@gmail.com>
2024-10-30 16:24:30 +0500

    Update examples3.py

--- examples/examples3.py
+++ examples/examples3.py
@@ -1,1 +1,10 @@
-
+print("Функция с одним аргументом:")
+def example(color):
+    if color == "green":
+        return "This is tree"
+    elif color == "blue":
+        return "This is sky"
+    else:
+        return "I don't know"
+what_is_it = example('blue')
+print(what_is_it)

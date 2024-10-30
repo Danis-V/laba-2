e5ce4aee8fac18f08c8c3cdefb4e7925709d2d64
Danis-V <danis.vakhitov.06@gmail.com>
2024-10-30 16:31:07 +0500

    Create examples7.py

---
+++ examples/examples7.py
@@ -0,0 +1,5 @@
+print("Получение/разбиение аргументов – ключевых слов с помощью символа *: ")
+def example_args(*args):
+    print('Positional argument tuple:', args)
+example_args()
+example_args(1, 2, 4, 'argument')

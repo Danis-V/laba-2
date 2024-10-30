29bfcba726c8199e2bf17195c2afa7d2ffb7eb1b
Danis-V <danis.vakhitov.06@gmail.com>
2024-10-30 16:29:06 +0500

    Create examples6.py

---
+++ examples/examples6.py
@@ -0,0 +1,5 @@
+print("Аргумент – ключевые слова:")
+def cat(name, color, age):
+    return {'name': name, 'color': color, 'age': age}
+my_cat = cat(color = 'Brown', age = 10, name = 'Anfisa')
+print(my_cat)
